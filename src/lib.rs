d158d378b49ac2bb7fbec554357bdaf0cb7ea684
Tamme Schichler <tamme@schichler.dev>
2020-05-23 19:06:42 +0200

    More Clippy configuration

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,24 @@
-//TODO: clippy::cargo
-#![warn(clippy::pedantic, clippy::as_conversions, clippy::clone_on_ref_ptr)]
+//TODO: clippy::cargo, clippy::missing_docs_in_private_items
+#![warn(
+    clippy::as_conversions,
+    clippy::clone_on_ref_ptr,
+    clippy::fallible_impl_from,
+    clippy::missing_const_for_fn,
+    clippy::multiple_crate_versions,
+    clippy::needless_borrow,
+    clippy::pedantic,
+    clippy::use_self,
+    clippy::wrong_pub_self_convention
+)]
+#![allow(clippy::wildcard_imports)]
+#![deny(clippy::wildcard_dependencies)]
+// Debug cleanup. Uncomment before committing.
+#![forbid(
+    clippy::dbg_macro,
+    clippy::print_stdout,
+    clippy::todo,
+    clippy::unimplemented
+)]
 
 pub mod rc;
 pub mod sync;
